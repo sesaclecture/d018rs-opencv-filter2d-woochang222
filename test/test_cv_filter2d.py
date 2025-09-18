ecc0ab66eada2eb26e003325cc0e356554c39ea9
WooChangKim <nanenchanga2@gmail.com>
2025-09-18 15:55:37 +0900

    a

--- test/test_cv_filter2d.py
+++ test/test_cv_filter2d.py
@@ -4,9 +4,9 @@ from src.cv_filter2d import identity_kernel, ones_kernel, original_kernel, doubl
 
 
 image = np.array([[1, 2, 3, 4],
-                  [5, 6, 7, 8],
+                  [5, 6, 7, 8], 
                   [9, 10, 11, 12],
-                  [13, 14, 15, 16]], dtype=np.float32)
+                  [13, 14, 15, 16]], dtype=np.float32)  
 
 
 def test_identity():
